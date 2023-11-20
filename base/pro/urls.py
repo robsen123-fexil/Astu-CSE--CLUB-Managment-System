4f596e9b5a75c8b1005f97790ea55259af503660
robsen123-fexil <111446547+robsen123-fexil@users.noreply.github.com>
2023-11-20 18:03:32 +0300

    setup

--- base/pro/urls.py
+++ base/pro/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-urlpatterns =[
+
+urlpatterns = [
     path('', views.login, name='login'),
-    path('register/', views.register , name='register')
+    path('register/', views.register, name='register'),
 ]
