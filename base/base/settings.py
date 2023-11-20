662ff3d28488b914ff5e40ff9d3b1a13c452a3f4
robsen123-fexil <111446547+robsen123-fexil@users.noreply.github.com>
2023-11-20 17:25:59 +0300

    setup

--- base/base/settings.py
+++ base/base/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'pro'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'base.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- base/pro/views.py
+++ base/pro/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+def register(request):
+    return render(request, 'register.html')
