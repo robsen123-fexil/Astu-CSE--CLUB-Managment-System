f77878124990dbffedf6036c87dac146097bdd05
robsen123-fexil <111446547+robsen123-fexil@users.noreply.github.com>
2023-11-20 23:54:33 +0300

    login and registration update

--- base/pro/urls.py
+++ base/pro/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('', views.login_view, name='login_view'),
     path('register/', views.register, name='register'),
     path('home/', views.home, name='home')
 ]
