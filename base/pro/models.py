6b89c6ab53808d64708627cfa9208982ea4f32cf
robsen123-fexil <111446547+robsen123-fexil@users.noreply.github.com>
2023-11-21 07:31:44 +0300

    register form

--- base/pro/models.py
+++ base/pro/models.py
@@ -5,3 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.CharField(max_length=10000000)
     date = models.DateTimeField(default=datetime.now, blank=True, editable=False)
+class register(models.Model):
+    name=models.CharField(max_length=100)
+    username=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    confirm_password=models.CharField(max_length=100)
--- base/pro/views.py
+++ base/pro/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from .models import Post
 from django.contrib.auth import authenticate
 from django.contrib.auth import authenticate , login 
-
+from django.contrib.auth.models import User , auth
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,6 +19,27 @@ def login_view(request):
 
     return render(request, 'login.html')
 def register(request):
-    return render(request, 'register.html')
+    if request.method=='POST':
+         name=request.POST['name']
+         username=request.POST['username']        
+         password=request.POST['password']
+         confirm_password=request.POST['password2']
+
+         if password == confirm_password:
+              if User.objects.filter(username=username).exists():
+                messages.info(request, "Username Already Taken")
+                return redirect('register')
+              elif User.objects.filter(name=name).exists():
+                  messages.info(request," This Name Is Already Registered ")    
+                  return redirect('register')
+              else:
+                  user=User.objects.create_user(name=name, username=username,password=password)
+                  user.save()
+                  return redirect('login')
+         else:
+              messages.info(request, "Password Is Not The Same")
+              return redirect('register')
+    else:   
+        return render(request, 'register.html')
 def home(request):
         return render(request, 'home.html')
